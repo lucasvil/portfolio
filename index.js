2f6530427297946f0d13c58369ba0ac4ad26e0bf
lucasvil <vilucas7@gmail.com>
2024-01-30 18:04:37 +0100

    add partials, minor bug fix for 'theme'

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const { engine } =require('express-handlebars');
 
 const app = express()
 
-const port = (process.env.NODE_ENV == 'production') ? 80: 3000
+const port = (process.env.NODE_ENV == 'production') ? 80 : 3000
 
-app.engine('handlebars', engine());
+app.engine('handlebars', engine({
+  partialsDir: __dirname + '/views/partials/'
+}));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
@@ -15,8 +17,16 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/project', (req, res) => {
-  res.render('project')
+app.get('/lsp', (req, res) => {
+  res.render('lsp')
+})
+
+app.get('/houseplant', (req, res) => {
+  res.render('houseplant')
+})
+
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
 })
 
 app.listen(port, () => {
