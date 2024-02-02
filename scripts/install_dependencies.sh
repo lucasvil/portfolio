b98e9a2e239f5886250d998f4d003a6776b9813f
lucasvil <vilucas7@gmail.com>
2024-02-02 14:54:06 +0100

    add scripts

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "install dependencies"
+echo 'cd /home/ec2-user/portfolio' >> /home/ec2-user/portfolio/deploy.log
+cd /home/ec2-user/portfolio/ >> /home/ec2-user/portfolio/deploy.log
+
+echo 'npm install' >> /home/ec2-user/portfolio/deploy.log
+npm install >> /home/ec2-user/portfolio/deploy.log
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "start server"
+echo 'cd /home/ec2-user/portfolio' >> /home/ec2-user/portfolio/deploy.log
+
+echo 'pm3 restart portfolio-express' >> /home/ec2-user/portfolio/deploy.log
+pm3 restart portfolio-express >> /home/ec2-user/portfolio/deploy.log
