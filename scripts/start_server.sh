978f0cd64d44802005418898589734352b2e7c3b
lucasvil <vilucas7@gmail.com>
2024-02-02 14:57:05 +0100

    fix typo

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,4 +2,4 @@
 echo 'cd /home/ec2-user/portfolio' >> /home/ec2-user/portfolio/deploy.log
 
 echo 'pm3 restart portfolio-express' >> /home/ec2-user/portfolio/deploy.log
-pm3 restart portfolio-express >> /home/ec2-user/portfolio/deploy.log
+pm2 restart portfolio-express >> /home/ec2-user/portfolio/deploy.log
