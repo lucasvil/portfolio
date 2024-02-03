00489c1b6829eb1e8e5a9cf03ede8150870bc364
lucasvil <vilucas7@gmail.com>
2024-02-04 00:54:49 +0100

    TLS

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express')
 const { engine } =require('express-handlebars');
 
 const app = express()
-
-const port = (process.env.NODE_ENV == 'production') ? 80 : 3000
 
 app.engine('handlebars', engine({
   partialsDir: __dirname + '/views/partials/'
@@ -29,6 +30,14 @@ app.get('/portfolio', (req, res) => {
   res.render('portfolio')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
+const httpServer = http.createServer(app);
+httpServer.listen(3000)
+
+if(process.env.NODE_ENV != 'dev'){
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/lucasvillarroel.dev/fullchain.pem', 'utf8');
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/lucasvillarroel.dev/privkey.pem', 'utf8');
+
+  const httpsServer = https.createServer({key:privateKey, cert:certificate}, app);
+  httpsServer.listen(3443)
+}
